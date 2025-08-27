9a42ab6eacf080e322f5a6efdfe253cea463516f
Johd F Linares <jhodflinares@gmail.com>
2025-08-27 12:37:00 -0500

    add shandcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Hola ecommerce TechStore</h1>
+      <button variant="destructive">Click</button>
     </div>
   );
 }
